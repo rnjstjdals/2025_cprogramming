3d4430812bd35872bfb061e64c64db71448d8784
rnjstjdals <2024028@eonyang.hs.kr>
2025-03-27 15:17:01 +0900

    Add files via upload

---
+++ P29.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    printf("%d\n", 10+3);
+    printf("%d\n", 10-3);
+    printf("%d\n", 10*3);
+    printf("%d\n", 10/3);
+    printf("%f\n", 10.0/3.0);
+    printf("%d\n", 10%3);
+    printf("%f\nm,"(float)10/4+0.5) 부동소수점점
+
+    return 0;
+}
---
+++ p62.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int n, cnt, i, num,maxcnt=0,ansn;
+int main(){
+	scanf("%d",&num);
+	n=1;
+	while(n<=num){
+
+	i=1;
+	cnt=0;
+	while(i<=n){
+		if(n%i==0){
+			printf("%d",i);
+			cnt++; 
+		}
+		i++;
+	}
+	printf("\n%d\n",cnt);
+	if(maxcnt<cnt){
+		maxcnt=cnt;
+		ansn=n;
+	}
+	
+	n++;
+}
+    printf("answer:%d, %d \n",ansn, maxcnt);
+	return 0;
+
+}
